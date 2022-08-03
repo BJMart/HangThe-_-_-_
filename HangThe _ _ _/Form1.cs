050578e973a31f78a62ace51b0b4a15bb0431b68
byron.martiensen <byron.martiensen@A08CFDF31EE2P>
2022-08-03 10:35:41 +1000

    Some cide

--- HangThe _ _ _/Form1.cs
+++ HangThe _ _ _/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace HangThe______
 {
@@ -15,6 +17,15 @@ namespace HangThe______
         public Form1()
         {
             InitializeComponent();
+
+            string path = "words.txt";
+
+            FileStream fs = File.OpenRead(path);
+            byte[] encodedString = new byte[1024];
+
+
+            byte[] data = Convert.FromBase64String(encodedString);
+            string decodedString = Encoding.UTF8.GetString(data);
         }
 
         private void button1_Click(object sender, EventArgs e)
